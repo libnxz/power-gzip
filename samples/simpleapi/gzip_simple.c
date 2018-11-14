55ff205269bd41535180a8bd69a64c33524dc1af
Bedri-Sendir2 <bedri.sendir2@ibm.com>
2018-11-14 12:35:04 -0600

    1- copies nx_touch_pages_dde and nx_append_dde from nx-zlib 2- handle syscall errors in p9open/p9close properly 3- adds dummy overflow buffer for inflate 4- copies sigserv handler from nx-zlib 5- fixes size issue in gzip trailer

--- samples/simpleapi/gzip_simple.c
+++ samples/simpleapi/gzip_simple.c
@@ -11,6 +11,9 @@ static void intcopy(int value, char *buffer)
 	buffer[0] = value & 0xFF;
 }
 
+/*
+   Touch specified number of pages in supplied buffer
+ */
 static int nx_touch_pages(void *buf, long buf_len, long page_len, int wr)
 {
 	char *begin = buf;
@@ -39,9 +42,208 @@ static int nx_touch_pages(void *buf, long buf_len, long page_len, int wr)
 	return 0;
 }
 
+/*
+   Touch specified number of pages represented in number bytes
+   beginning from the first buffer in a dde list.
+   Do not touch the pages past buf_sz-th byte's page.
+
+   Set buf_sz = 0 to touch all pages described by the ddep.
+*/
+static int nx_touch_pages_dde(nx_dde_t *ddep, long buf_sz, long page_sz, int wr)
+{
+	volatile char t;
+	uint32_t indirect_count;
+	uint32_t buf_len;
+	long total;
+	uint64_t buf_addr;
+	nx_dde_t *dde_list;
+	int i;
+
+	assert(!!ddep);
+
+	nx_touch_pages((void *)ddep, sizeof(nx_dde_t), page_sz, 0);
+
+	indirect_count = getpnn(ddep, dde_count);
+
+	// prt_trace("nx_touch_pages_dde dde_count %d request len 0x%lx\n",
+	// indirect_count, buf_sz);
+
+	if (indirect_count == 0) {
+		/* direct dde */
+		buf_len = getp32(ddep, ddebc);
+		buf_addr = getp64(ddep, ddead);
+
+		// prt_trace("touch direct ddebc 0x%x ddead %p\n", buf_len,
+		// (void *)buf_addr);
+
+		if (buf_sz == 0)
+			nx_touch_pages((void *)buf_addr, buf_len, page_sz, wr);
+		else
+			nx_touch_pages((void *)buf_addr,
+				       NX_MIN(buf_len, buf_sz), page_sz, wr);
+
+		return ERR_NX_OK;
+	}
+
+	/* indirect dde */
+	if (indirect_count > MAX_DDE_COUNT)
+		return ERR_NX_EXCESSIVE_DDE;
+
+	/* first address of the list */
+	dde_list = (nx_dde_t *)getp64(ddep, ddead);
+
+	if (buf_sz == 0)
+		buf_sz = getp32(ddep, ddebc);
+
+	total = 0;
+	for (i = 0; i < indirect_count; i++) {
+		buf_len = get32(dde_list[i], ddebc);
+		buf_addr = get64(dde_list[i], ddead);
+		total += buf_len;
+
+		nx_touch_pages((void *)&(dde_list[i]), sizeof(nx_dde_t),
+			       page_sz, 0);
+
+		// prt_trace("touch loop len 0x%x ddead %p total 0x%lx\n",
+		// buf_len, (void *)buf_addr, total);
+
+		/* touching fewer pages than encoded in the ddebc */
+		if (total > buf_sz) {
+			buf_len = NX_MIN(buf_len, total - buf_sz);
+			nx_touch_pages((void *)buf_addr, buf_len, page_sz, wr);
+			// prt_trace("touch loop break len 0x%x ddead %p\n",
+			// buf_len, (void *)buf_addr);
+			break;
+		}
+		nx_touch_pages((void *)buf_addr, buf_len, page_sz, wr);
+	}
+	return ERR_NX_OK;
+}
+
+
+/*
+   Adds an (address, len) pair to the list of ddes (ddl) and updates
+   the base dde.  ddl[0] is the only dde in a direct dde which
+   contains a single (addr,len) pair.  For more pairs, ddl[0] becomes
+   the indirect (base) dde that points to a list of direct ddes.
+   See Section 6.4 of the NX-gzip user manual for DDE description.
+   Addr=NULL, len=0 clears the ddl[0].  Returns the total number of
+   bytes in ddl.  Caller is responsible for allocting the array of
+   nx_dde_t *ddl.  If N addresses are required in the scatter-gather
+   list, the ddl array must have N+1 entries minimum.
+*/
+int nx_append_dde(nx_dde_t *ddl, void *addr, uint32_t len)
+{
+	uint32_t ddecnt;
+	uint32_t bytes;
+
+	if (addr == NULL && len == 0) {
+		clearp_dde(ddl);
+		return 0;
+	}
+
+	// prt_trace("%d: nx_append_dde addr %p len %x\n", __LINE__, addr, len);
+
+	/* number of ddes in the dde list ; == 0 when it is a direct dde */
+	ddecnt = getpnn(ddl, dde_count);
+	bytes = getp32(ddl, ddebc);
+
+	/* NXPRT( fprintf(stderr, "%d: get dde_count %d ddebc %d\n", __LINE__,
+	 * ddecnt, bytes ) ); */
+
+	if (ddecnt == 0 && bytes == 0) {
+		/* first dde is unused; make it a direct dde */
+		bytes = len;
+		putp32(ddl, ddebc, bytes);
+		putp64(ddl, ddead, (uint64_t)addr);
+
+		/* NXPRT( fprintf(stderr, "%d: put ddebc %d ddead %p\n",
+		 * __LINE__, bytes, (void *)addr ) ); */
+	} else if (ddecnt == 0) {
+		/* converting direct to indirect dde */
+		/* ddl[0] becomes head dde of ddl */
+		/* copy direct to indirect first */
+		ddl[1] = ddl[0];
+
+		/* add the new dde next */
+		clear_dde(ddl[2]);
+		put32(ddl[2], ddebc, len);
+		put64(ddl[2], ddead, (uint64_t)addr);
+
+		/* ddl head points to 2 direct ddes */
+		ddecnt = 2;
+		putpnn(ddl, dde_count, ddecnt);
+		bytes = bytes + len;
+		putp32(ddl, ddebc, bytes);
+		/* pointer to the first direct dde */
+		putp64(ddl, ddead, (uint64_t)&ddl[1]);
+	} else {
+		/* append a dde to an existing indirect ddl */
+		++ddecnt;
+		clear_dde(ddl[ddecnt]);
+		put64(ddl[ddecnt], ddead, (uint64_t)addr);
+		put32(ddl[ddecnt], ddebc, len);
+
+		putpnn(ddl, dde_count, ddecnt);
+		bytes = bytes + len;
+		putp32(ddl, ddebc, bytes); /* byte sum of all dde */
+	}
+	return bytes;
+}
+
+void sigsegv_handler(int sig, siginfo_t *info, void *ctx)
+{
+	fprintf(stderr, "%d: Got signal %d si_code %d, si_addr %p\n", getpid(),
+		sig, info->si_code, info->si_addr);
+	nx_fault_storage_address = info->si_addr;
+	exit(0);
+}
+
+static void nx_print_dde(nx_dde_t *ddep, const char *msg)
+{
+	uint32_t indirect_count;
+	uint32_t buf_len;
+	uint64_t buf_addr;
+	nx_dde_t *dde_list;
+	int i;
+
+	assert(!!ddep);
+	indirect_count = getpnn(ddep, dde_count);
+	buf_len = getp32(ddep, ddebc);
+	fprintf(stderr, "%s dde %p dde_count %d, ddebc 0x%x\n", msg, ddep,
+		indirect_count, buf_len);
+
+	if (indirect_count == 0) {
+		/* direct dde */
+		buf_len = getp32(ddep, ddebc);
+		buf_addr = getp64(ddep, ddead);
+		fprintf(stderr, "  direct dde: ddebc 0x%x ddead %p %p\n",
+			buf_len, (void *)buf_addr, (void *)buf_addr + buf_len);
+		return;
+	}
+
+	/* indirect dde */
+	if (indirect_count > MAX_DDE_COUNT) {
+		fprintf(stderr, "  error MAX_DDE_COUNT\n");
+		return;
+	}
+
+	/* first address of the list */
+	dde_list = (nx_dde_t *)getp64(ddep, ddead);
+
+	for (i = 0; i < indirect_count; i++) {
+		buf_len = get32(dde_list[i], ddebc);
+		buf_addr = get64(dde_list[i], ddead);
+		fprintf(stderr, " indirect dde: ddebc 0x%x ddead %p %p\n",
+			buf_len, (void *)buf_addr, (void *)buf_addr + buf_len);
+	}
+	return;
+}
+
+
 /*zero out csb and init src and dst*/
 void nx_init_csb(nx_gzip_crb_cpb_t *cmdp, void *src, void *dst, int srclen,
-		 int dstlen)
+		 int dstlen, nx_dde_t *ddl, void *overflow)
 {
 	/* status, output byte count in tpbc */
 	put64(cmdp->crb, csb_address, 0);
@@ -54,11 +256,23 @@ void nx_init_csb(nx_gzip_crb_cpb_t *cmdp, void *src, void *dst, int srclen,
 	put32(cmdp->crb.source_dde, ddebc, srclen);
 	put64(cmdp->crb.source_dde, ddead, (uint64_t)src);
 
-	/* target direct dde */
-	clear_dde(cmdp->crb.target_dde);
-	putnn(cmdp->crb.target_dde, dde_count, 0);
-	put32(cmdp->crb.target_dde, ddebc, dstlen);
-	put64(cmdp->crb.target_dde, ddead, (uint64_t)dst);
+	if (ddl != NULL) {
+		/*switch to indirect dde & append a dummy overflow buffer*/
+		clearp_dde(ddl);
+		nx_append_dde(ddl, (uint64_t)dst, dstlen);
+		nx_append_dde(ddl, (uint64_t)overflow, OVERFLOW_BUFFER_SIZE);
+		cmdp->crb.target_dde = *ddl;
+		/*source is always direct -- we don't need to touch*/
+		/*we touch target dde because of the overflow buffer we supply*/
+		nx_touch_pages_dde(ddl, dstlen + OVERFLOW_BUFFER_SIZE, pagesize,
+				   1);
+	} else {
+		/* target direct dde */
+		clear_dde(cmdp->crb.target_dde);
+		putnn(cmdp->crb.target_dde, dde_count, 0);
+		put32(cmdp->crb.target_dde, ddebc, dstlen);
+		put64(cmdp->crb.target_dde, ddead, (uint64_t)dst);
+	}
 }
 
 /******************/
@@ -68,8 +282,24 @@ void nx_init_csb(nx_gzip_crb_cpb_t *cmdp, void *src, void *dst, int srclen,
 /*open device*/
 p9_simple_handle_t *p9open()
 {
-	p9_simple_handle_t *myhandle = malloc(sizeof(p9_simple_handle_t));
+	p9_simple_handle_t *myhandle =
+		malloc(sizeof(p9_simple_handle_t) + OVERFLOW_BUFFER_SIZE);
 	myhandle->dev_handle = nx_function_begin(NX_FUNC_COMP_GZIP, 0);
+
+	myhandle->overflow_buffer = myhandle + sizeof(gzip_dev *);
+	if (myhandle->dev_handle == NULL) {
+		free(myhandle);
+		return NULL;
+	}
+
+	/* add a signal action */
+	sigact.sa_handler = 0;
+	sigact.sa_sigaction = sigsegv_handler;
+	sigact.sa_flags = SA_SIGINFO;
+	sigact.sa_restorer = 0;
+	sigemptyset(&sigact.sa_mask);
+	sigaction(SIGSEGV, &sigact, NULL);
+
 	return myhandle;
 };
 
@@ -118,17 +348,18 @@ int p9deflate(p9_simple_handle_t *handle, void *src, void *dst, int srclen,
 	putnn(cmdp->crb, gzip_fc, GZIP_FC_COMPRESS_FHT);
 
 	/*set source/destination and sizes*/
-	nx_init_csb(cmdp, src, dstp + header, srclen,
-		    dstlen - header - trailer);
-
+	nx_init_csb(cmdp, src, dstp + header, srclen, dstlen - header - trailer,
+		    NULL, NULL);
 	/*touch pages before submitting job*/
 	nx_touch_pages(src, srclen, pagesize, 0);
 	nx_touch_pages(dstp, dstlen, pagesize, 1);
 
 	/*run the job */
-	nxu_run_job(cmdp, handle->dev_handle);
+	cc = nxu_run_job(cmdp, handle->dev_handle);
 
-	cc = getnn(cmdp->crb.csb, csb_cc);
+	if (!cc) {
+		cc = getnn(cmdp->crb.csb, csb_cc);
+	}
 
 	if (cc != ERR_NX_OK && cc != ERR_NX_TPBC_GT_SPBC
 	    && cc != ERR_NX_TRANSLATION) {
@@ -149,7 +380,7 @@ int p9deflate(p9_simple_handle_t *handle, void *src, void *dst, int srclen,
 		/*gzip trailer, crc32 and compressed data size*/
 		int crc32 = cmdp->cpb.out_crc;
 		intcopy(crc32, dstp + header + comp);
-		intcopy(comp, dstp + header + comp + 4);
+		intcopy(srclen, dstp + header + comp + 4);
 	} else if (flag == ZLIB_WRAPPER) {
 		/*zlib trailer, adler32 only*/
 		int adler32 = cmdp->cpb.out_adler;
@@ -195,23 +426,34 @@ int p9inflate(p9_simple_handle_t *handle, void *src, void *dst, int srclen,
 	cmdp->crb.gzip_fc = 0;
 	putnn(cmdp->crb, gzip_fc, GZIP_FC_DECOMPRESS);
 
-	/*set source/destination and sizes*/ // TODO
-	nx_init_csb(cmdp, src + header, dst, srclen - header - trailer,
-		    dstlen + 1);
+	/*allocate a ddl on stack for a dummy buffer*/
+	nx_dde_t ddl[3];
+
+	/*set source/destination and sizes*/
+	nx_init_csb(cmdp, src + header, dst, srclen - header - trailer, dstlen,
+		    ddl, handle->overflow_buffer);
+
+#ifdef P9DBG
+	/*print dde*/
+	nx_print_dde(ddl, "inflate test");
+#endif
 
 	/*touch all pages before submitting job*/
 	nx_touch_pages(src, srclen, pagesize, 0);
 	nx_touch_pages(dst, dstlen, pagesize, 1);
 
 	/*run the job*/
-	nxu_run_job(cmdp, handle->dev_handle);
+	cc = nxu_run_job(cmdp, handle->dev_handle);
 
-	cc = getnn(cmdp->crb.csb, csb_cc);
+	if (!cc) {
+		cc = getnn(cmdp->crb.csb, csb_cc);
+	}
 
 	if (cc != ERR_NX_OK) {
 		fprintf(stderr, "nx inflate error: cc= %d\n", cc);
 		return -1;
 	}
+
 	int uncompressed = get32(cmdp->crb.csb, tpbc);
 
 #ifdef P9DBG
@@ -220,16 +462,19 @@ int p9inflate(p9_simple_handle_t *handle, void *src, void *dst, int srclen,
 		int crc32 = cmdp->cpb.out_crc;
 		int s_crc32 = *(int *)(srcp + srclen - trailer);
 		int size = *(int *)(srcp + srclen - trailer + 4);
-		printf("crc computed on inflate=%d - crc read from trailer=%d\n",
-		       crc32, s_crc32);
-		printf("size stored in trailer=%d reading crc from location:%d\n",
-		       size, srclen - trailer);
+		fprintf(stdout,
+			"crc computed from src trailer=%d - cbp.out_crc computed=%d\n",
+			crc32, s_crc32);
+		fprintf(stdout,
+			"size read from src trailer=%d - tbpc computed:=%d\n",
+			size, uncompressed);
 	} else if (flag == ZLIB_WRAPPER) {
 		/*zlib trailer, adler32 only*/
 		int adler32 = cmdp->cpb.out_adler;
 		int s_adler32 = (int)srcp[srclen - trailer];
-		printf("adler computed on inflate=%d adler read from header=%d\n",
-		       adler32, s_adler32);
+		fprintf(stdout,
+			"adler computed on inflate=%d adler read from header=%d\n",
+			adler32, s_adler32);
 	}
 #endif
 
@@ -239,7 +484,16 @@ int p9inflate(p9_simple_handle_t *handle, void *src, void *dst, int srclen,
 /*close the compressor*/
 int p9close(p9_simple_handle_t *handle)
 {
-	int retval = nx_function_end(handle->dev_handle);
+	int retval = 0;
+	if (handle != NULL && handle->dev_handle != NULL) {
+		retval = nx_function_end(handle->dev_handle);
+	}
+
+	if (retval < 0) {
+		return retval;
+	}
+
 	free(handle);
-	return retval; // TODO error check from function_end
+	handle = NULL;
+	return retval;
 };

cd65daf7e3d705e78bc4e632da88497f5838b919
Yi Li <liyiadam@gmail.com>
2019-05-11 21:26:38 +0800

    Revert "Skip the four-byte checksum in zlib."
    This reverts commit 5e7ace7c7b78c988e03d9081a0061ea4a4ba64e4.
    This commit causes core dump in mariaDB test.

--- nx_inflate.c
+++ nx_inflate.c
@@ -731,15 +731,11 @@ small_next_in:
 			update_stream_in(s, read_sz);
 			update_stream_in(s->zstrm, read_sz);
 			s->used_in = s->used_in + read_sz;
-			if (s->wrap == HEADER_ZLIB && s->is_final == 1 && s->used_in == 4)
-				/* TODO: four-byte Adler-32 checksum */
-				return Z_STREAM_END;
 		}
 		else {
-			/* should not come here, but it's not an error if reach here */
-			prt_warn("unexpected warning s->avail_in %d s->len_in %d s->cur_in %d s->used_in %d\n",
-				s->avail_in, s->len_in, s->cur_in, s->used_in);
-			return Z_OK;
+			/* should never come here */
+			prt_err("unexpected error\n");
+			return Z_STREAM_END;
 		}
 	}
 	else {
@@ -1105,14 +1101,7 @@ offsets_state:
 		s->used_in = 0;
 		if (s->used_out == 0) {
 			print_dbg_info(s, __LINE__);
-			if (s->avail_in == 4) {
-				/* TODO skip 4-byte ADLER32 checksum */
-				update_stream_in(s, 4);
-				update_stream_in(s->zstrm, 4);
-			}
-
-			if (s->is_final == 1) return Z_STREAM_END;
-			else return Z_OK;
+			return Z_STREAM_END;
 		}
 		else
 			goto copy_fifo_out_to_next_out;

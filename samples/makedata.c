313fd2974d22857a43eae63bc7ca4f5b3c0258e4
Bulent Abali <abali@us.ibm.com>
2019-08-18 14:17:41 -0400

    random file maker

---
+++ samples/makedata.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+/* change this to the output file size you desire */
+#define OUTFILESZ (1<<20)
+
+int main(int argc, char **argv)
+{
+	size_t bufsz = OUTFILESZ;
+	size_t readsz, writesz;
+	char *buf;
+	size_t idx;
+	int seed;
+
+	if (argc == 3 && strcmp(argv[1], "-s") == 0) {
+		seed = atoi(argv[2]);
+	}	
+	if ((argc == 1) || (argc == 2 && strcmp(argv[1], "-h") == 0)) {
+		fprintf(stderr,"randomly produces a data file using a seed number and any input file\n");
+		fprintf(stderr,"usage: %s -s rngseed < seedfile > outputfile\n", argv[0]);
+		return -1;
+	}
+	
+	assert(NULL != (buf = malloc(bufsz)));
+
+	/* read up to half the buffer size */
+	readsz = fread(buf, 1, bufsz/2, stdin);
+	fprintf(stderr, "read bytes %ld\n", readsz);
+
+	/* next free location */
+	idx = readsz;
+	
+	srand48(seed);
+
+	size_t len_max = lrand48() % 240;
+
+	while(idx < bufsz) {
+
+		/* pick random point in the buffer and copy */
+		size_t dist = lrand48() % idx;
+		size_t len = (lrand48() % len_max) + 16;
+
+		if (dist > idx) 
+			dist = idx; /* out of bounds */
+
+		/* copy */
+		while (len-- > 0 && idx < bufsz) {
+			buf[idx] = buf[idx-dist];
+			++idx;
+		}
+	}
+
+	writesz = fwrite(buf, 1, idx, stdout);
+		
+	return 0;
+}
+

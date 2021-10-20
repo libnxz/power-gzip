a5f741cad09e580456580c04de734edf5685252d
Tulio Magno Quites Machado Filho <tuliom@linux.ibm.com>
2021-10-20 15:06:00 -0300

    Return the compressed length from _test[_nx]_deflate
    After deflating, return the length of the compressed stream in order to
    use that information when calling the inflate functions.
    When compressing data, the ending data may be shorter, equal or larger
    than the original data, requiring to pass the correct value to inflate later.
    
    Signed-off-by: Tulio Magno Quites Machado Filho <tuliom@linux.ibm.com>

--- test/deflate/hello.c
+++ test/deflate/hello.c
@@ -7,7 +7,7 @@ static z_const char hello[] = "hello, hello!";
 int run_case1()
 {
 	Byte *compr, *uncompr;
-	uLong comprLen = 10000*sizeof(int);
+	unsigned int comprLen = 10000*sizeof(int);
 	uLong uncomprLen = comprLen;
 	uLong len = (uLong) strlen(hello) + 1;
 	compr = (Byte*)calloc((uInt)comprLen, 1);
@@ -19,7 +19,9 @@ int run_case1()
 	}
 
 	for (int i = 0; i < 30; i++) {
-		if (_test_nx_deflate((Byte *)hello, len, compr, comprLen, 1))
+		/* Reset comprLen. */
+		comprLen = 10000*sizeof(int);
+		if (_test_nx_deflate((Byte *)hello, len, compr, &comprLen, 1))
 			return TEST_ERROR;
 		if (_test_inflate(compr, comprLen, uncompr, uncomprLen,
 				  (Byte *) hello, len, 1))
--- test/test_utils.h
+++ test/test_utils.h
@@ -7,9 +7,9 @@ extern void generate_all_data(int len, char digit);
 extern int compare_data(char* src, char* dest, int len);
 
 int _test_nx_deflate(Byte* src, unsigned int src_len, Byte* compr,
-		     unsigned int compr_len, int step);
+		     unsigned int* compr_len, int step);
 int _test_deflate(Byte* src, unsigned int src_len, Byte* compr,
-		  unsigned int compr_len, int step);
+		  unsigned int* compr_len, int step);
 int _test_inflate(Byte* compr, unsigned int comprLen, Byte* uncompr,
 		  unsigned int uncomprLen, Byte* src, unsigned int src_len,
 		  int step);

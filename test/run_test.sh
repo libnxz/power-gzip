81de594bbae04e0a9739edbd519c8d34563fc062
Matheus Castanho <msc@linux.ibm.com>
2021-10-28 14:44:51 -0300

    Add test for deflate with zero input data
    This test aims to be a more suitable replacement for samples/zpipe-issue99.c,
    formatted as a test case instead of a sample program.
    
    It checks deflate return values for different flush parameters when no input is
    given (similar to zpipe-issue99), but also tests 0-length files (issue #76).
    
    The expected return values are based on the output from zlib. Specially for the
    empty file tests, the output stream from zlib and libnxz will probably be
    different, which is OK. But the return values of the intermediate calls to
    deflate should be equivalent.
    
    Signed-off-by: Matheus Castanho <msc@linux.ibm.com>

--- test/run_test.sh
+++ test/run_test.sh
@@ -53,6 +53,12 @@ if [ $? -ne 0 ]; then
     echo "test_pid_reuse failed."
     exit 1;
 fi
+echo "running test_zeroinput..."
+$TEST_WRAPPER ./test_zeroinput >> $run_test_log 2>&1
+if [ $? -ne 0 ]; then
+    echo "test_zeroinput failed."
+    exit 1;
+fi
 echo "------------------------------"
 
 grep -E 'run_case|failed' $run_test_log | tee -a $run_test_report

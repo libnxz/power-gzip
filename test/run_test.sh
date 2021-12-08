6be8109686d81d0937413e9203c180e871c315ce
Matheus Castanho <msc@linux.ibm.com>
2021-12-08 17:34:54 -0300

    Improve code reuse in run_test.sh
    Signed-off-by: Matheus Castanho <msc@linux.ibm.com>

--- test/run_test.sh
+++ test/run_test.sh
@@ -8,63 +8,29 @@ run_test_report=run_test_report_${TS}.txt
 > $run_test_log
 > $run_test_report
 
-echo "running test_deflate..."
-$TEST_WRAPPER ./test_deflate >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_deflate failed."
-    exit 1;
-fi
-echo "running test_inflate..."
-$TEST_WRAPPER ./test_inflate >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_inflate failed."
-    exit 1;
-fi
-echo "running test_inflatesyncpoint..."
-$TEST_WRAPPER ./test_inflatesyncpoint >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_inflatesyncpoint failed."
-    exit 1;
-fi
-echo "running test_stress..."
-$TEST_WRAPPER ./test_stress  >> $run_test_log 2>&1
-echo "running test_crc32..."
-$TEST_WRAPPER ./test_crc32   >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_crc32 failed."
-    exit 1;
-fi
-echo "running test_adler32..."
-$TEST_WRAPPER ./test_adler32 >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_adler32 failed."
-    exit 1;
-fi
-echo "running test_multithread_stress..."
-nthreads=${TEST_NTHREADS:-$(nproc)}
-$TEST_WRAPPER ./test_multithread_stress $nthreads 10 6  >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_multithread_stress failed."
-    exit 1;
-fi
-echo "running test_pid_reuse..."
-$TEST_WRAPPER ./test_pid_reuse 2 >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_pid_reuse failed."
-    exit 1;
-fi
-echo "running test_zeroinput..."
-$TEST_WRAPPER ./test_zeroinput >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_zeroinput failed."
-    exit 1;
-fi
-echo "running test_buf_error..."
-$TEST_WRAPPER ./test_buf_error >> $run_test_log 2>&1
-if [ $? -ne 0 ]; then
-    echo "test_buf_error failed."
-    exit 1;
-fi
+run() {
+    test="$1"
+    shift 1
+    testname=$(basename $test)
+
+    echo "running ${testname}..."
+    $TEST_WRAPPER $test "$@" >> $run_test_log 2>&1
+    if [ $? -ne 0 ]; then
+        echo "${testname} failed."
+        exit 1;
+    fi
+}
+
+run ./test_deflate
+run ./test_inflate
+run ./test_inflatesyncpoint
+run ./test_stress
+run ./test_crc32
+run ./test_adler32
+run ./test_multithread_stress ${TEST_NTHREADS:-$(nproc)} 10 6
+run ./test_pid_reuse 2
+run ./test_zeroinput
+run ./test_buf_error
 echo "------------------------------"
 
 grep -E 'run_case|failed' $run_test_log | tee -a $run_test_report

libnxz/power-gzip

commit 0c6cdfd868cc471dd0c4632e6f5f9f6f518fa61b

Remove nx_fault_storage_address and related code
This variable is a relic from older code that used a signal handler to
catch page faults and set this variable to trigger touching of such
pages before re-issuing an NX job. libnxz doesn't use such mechanism
anymore because it touches pages ahead of job submission, so faults
shouldn't happen. In the end, codepaths depending on the value of
nx_fault_storage_address are just dead code, and can be safely removed.

Signed-off-by: Matheus Castanho <msc@linux.ibm.com>

lib/gzip_vas.c
@@ -90,7 +90,6 @@
    before checking if VAS window may be suspended. */
 #define SUSPENDED_TIME_THRESHOLD (nx_get_freq() >> 5)
 
-void *nx_fault_storage_address;
 uint64_t tb_freq=0;
 
 static const uint64_t timeout_seconds = 60;
@@ -275,11 +274,6 @@ static int nx_wait_for_csb( nx_gzip_crb_cpb_t *cmdp )
 		if (t > (timeout_seconds * onesecond)) /* 1 min */
 			break;
 
-		/* fault address from signal handler */
-		if( nx_fault_storage_address ) {
-			return -EAGAIN;
-		}
-
 		hwsync();
 	} while (getnn( cmdp->crb.csb, csb_v ) == 0);
 
@@ -326,16 +320,6 @@ int nxu_run_job(nx_gzip_crb_cpb_t *cmdp, nx_devp_t nxhandle)
 
 			if (!ret) {
 				return ret;
-			}
-			else if (ret == -EAGAIN) {
-				volatile long x;
-				prt_err("Touching address %p, 0x%lx\n",
-					 nx_fault_storage_address,
-					 *(long *)nx_fault_storage_address);
-				x = *(long *)nx_fault_storage_address;
-				*(long *)nx_fault_storage_address = x;
-				nx_fault_storage_address = 0;
-				continue;
 			}
 			else {
 				prt_err("wait_for_csb() returns %d\n", ret);

lib/nx_zlib.h
@@ -587,7 +587,6 @@ extern unsigned long crc32_ppc(unsigned long crc, const unsigned char *buffer,
                                unsigned long len);
 
 /* gzip_vas.c */
-extern void *nx_fault_storage_address;
 extern int nx_function_begin(int function, int pri, nx_devp_t nxhandle);
 extern int nx_function_end(nx_devp_t nxhandle);
 extern uint64_t nx_wait_ticks(uint64_t ticks, uint64_t accumulated_ticks, int do_sleep);

commit 658f5fa576349bf7a65e1c07509d71181385647d

Remove segmentation fault handler
Relic code used to use signal handlers in order to detect segmentation
faults.  This is not used anymore.

Fixes: 0c6cdfd868cc ("Remove nx_fault_storage_address and related code")

Signed-off-by: Tulio Magno Quites Machado Filho <tuliom@linux.ibm.com>

lib/gzip_vas.c
@@ -56,7 +56,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include <zlib.h>
 #include "nx-gzip.h"
 #include "crb.h"

lib/nx_zlib.c
@@ -51,7 +51,6 @@
 #include <sys/stat.h>
 #include <endian.h>
 #include <pthread.h>
-#include <signal.h>
 #include <dirent.h>
 #include <syslog.h>
 #include <limits.h>

lib/sw_zlib.c
@@ -49,7 +49,6 @@
 #include <sys/ioctl.h>
 #include <endian.h>
 #include <pthread.h>
-#include <signal.h>
 #include <zlib.h>
 #include <dlfcn.h>
 #include "nx_dbg.h"

samples/compdecomp.c
@@ -62,7 +62,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include "zlib.h"
 
 /* Caller must free the allocated buffer

samples/compdecomp_th.c
@@ -69,7 +69,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include <pthread.h>
 #include "zlib.h"
 

samples/gunzip_nx.c
@@ -56,7 +56,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include "nxu.h"
 #include "nx_dbg.h"
 #include "nx_zlib.h"
@@ -84,15 +83,6 @@ const int page_sz = 1<<16;
 const int line_sz = 1<<7;
 const int window_max = 1<<15;
 const int retry_max = 50;
-
-void sigsegv_handler(int sig, siginfo_t *info, void *ctx)
-{
-	fprintf(stderr, "%d: Got signal %d si_code %d, si_addr %p\n", getpid(),
-	       sig, info->si_code, info->si_addr);
-
-	_nx_fault_storage_address = info->si_addr;
-	/* exit(0); */
-}
 
 __attribute__ ((unused))
 static void nx_print_dde(nx_dde_t *ddep, const char *msg)
@@ -1198,20 +1188,12 @@ ok1:
 int main(int argc, char **argv)
 {
     int rc;
-    struct sigaction act;
     z_stream strm;
     nx_streamp s;
 
     strm.zalloc = (alloc_func) 0;
     strm.zfree = (free_func) 0;
     strm.opaque = (voidpf) 0;
-
-    act.sa_handler = 0;
-    act.sa_sigaction = sigsegv_handler;
-    act.sa_flags = SA_SIGINFO;
-    act.sa_restorer = 0;
-    sigemptyset(&act.sa_mask);
-    sigaction(SIGSEGV, &act, NULL);
 
     if (nx_deflateInit(&strm, Z_DEFAULT_COMPRESSION) != Z_OK) {
 	fprintf(stderr, "Unable to init NX, errno %d\n", errno);

samples/gzip_nxdht.c
@@ -60,7 +60,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include "nxu.h"
 #include "nx_dht.h"
 #include "nx_dbg.h"
@@ -477,32 +476,15 @@ int compress_file(int argc, char **argv, nx_devp_t handle, void *dhthandle)
 	return 0;
 }
 
-void sigsegv_handler(int sig, siginfo_t *info, void *ctx)
-{
-	fprintf(stderr, "%d: Got signal %d si_code %d, si_addr %p\n", getpid(),
-		sig, info->si_code, info->si_addr);
-
-	exit(-1);
-	nx_fault_storage_address = info->si_addr;
-}
-
 int main(int argc, char **argv)
 {
 	int rc;
-	struct sigaction act;
 	z_stream strm;
 	nx_streamp s;
 
 	strm.zalloc = (alloc_func) 0;
 	strm.zfree = (free_func) 0;
 	strm.opaque = (voidpf) 0;
-
-	act.sa_handler = 0;
-	act.sa_sigaction = sigsegv_handler;
-	act.sa_flags = SA_SIGINFO;
-	act.sa_restorer = 0;
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGSEGV, &act, NULL);
 
 	if (nx_deflateInit(&strm, Z_DEFAULT_COMPRESSION) != Z_OK) {
 		fprintf(stderr, "Unable to init NX, errno %d\n", errno);

samples/gzip_nxfht.c
@@ -60,7 +60,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include <limits.h>
 #include "nxu.h"
 #include "nx_dbg.h"
@@ -77,8 +76,6 @@ struct _nx_time_dbg {
 	uint64_t touch1, touch2;
 	uint64_t fault;
 } td;
-
-void *nx_fault_storage_address;
 
 #define SYSFS_GZIP_CAPS "/sys/devices/vio/ibm,compression-v1/nx_gzip_caps/"
 static int nx_query_job_limits()
@@ -441,32 +438,15 @@ int compress_file(int argc, char **argv, nx_devp_t handle)
 	return 0;
 }
 
-void sigsegv_handler(int sig, siginfo_t *info, void *ctx)
-{
-	fprintf(stderr, "%d: Got signal %d si_code %d, si_addr %p\n", getpid(),
-		sig, info->si_code, info->si_addr);
-
-	nx_fault_storage_address = info->si_addr;
-}
-
-
 int main(int argc, char **argv)
 {
 	int rc;
-	struct sigaction act;
 	z_stream strm;
 	nx_streamp s;
 
 	strm.zalloc = (alloc_func) 0;
 	strm.zfree = (free_func) 0;
 	strm.opaque = (voidpf) 0;
-
-	act.sa_handler = 0;
-	act.sa_sigaction = sigsegv_handler;
-	act.sa_flags = SA_SIGINFO;
-	act.sa_restorer = 0;
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGSEGV, &act, NULL);
 
 	if (nx_deflateInit(&strm, Z_DEFAULT_COMPRESSION) != Z_OK) {
 		fprintf(stderr, "Unable to init NX, errno %d\n", errno);

samples/nxlite.c
@@ -60,7 +60,6 @@
 #include <sys/ioctl.h>
 #include <assert.h>
 #include <errno.h>
-#include <signal.h>
 #include <stdarg.h>
 #include "nxu.h"
 #include "nx_dht.h"
@@ -84,10 +83,8 @@ typedef struct nxlite_handle_t {
 #endif
 #define NX_CHUNK_SZ  (1<<18)
 
-extern void *nx_fault_storage_address;
 extern void *nx_function_begin(int function, int pri);
 extern int nx_function_end(void *handle);
-static void sigsegv_handler(int sig, siginfo_t *info, void *ctx);
 
 static int compress_dht_sample(char *src, uint32_t srclen, char *dst,
 			       uint32_t dstlen, int with_count,
@@ -449,7 +446,6 @@ void nxlite_end(void *nxhandle)
 void *nxlite_begin()
 {
 	int rc;
-	struct sigaction act;
 	nxlite_handle_t *h;
 
 	if (NULL == (h = malloc(sizeof(nxlite_handle_t)))) {
@@ -471,14 +467,6 @@ void *nxlite_begin()
 		nxlite_end(h);
 		return NULL;
 	}
-
-	/* use signals for catching CSB page faults */
-	act.sa_handler = 0;
-	act.sa_sigaction = sigsegv_handler;
-	act.sa_flags = SA_SIGINFO;
-	act.sa_restorer = 0;
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGSEGV, &act, NULL);
 
 	return h;
 }
@@ -691,15 +679,6 @@ static int nxlite_compress(char *dest, uint64_t *destLen,
 
 	return Z_OK;
 }
-
-static void sigsegv_handler(int sig, siginfo_t *info, void *ctx)
-{
-	fprintf(stderr, "%d: Got signal %d si_code %d, si_addr %p\n", getpid(),
-		sig, info->si_code, info->si_addr);
-
-	nx_fault_storage_address = info->si_addr;
-}
-
 
 /*
   Compresses the source buffer into the destination buffer.  sourceLen
@@ -1448,15 +1427,7 @@ ok1:
 int main(int argc, char **argv)
 {
     int rc;
-    struct sigaction act;
     void *handle;
-
-    act.sa_handler = 0;
-    act.sa_sigaction = sigsegv_handler;
-    act.sa_flags = SA_SIGINFO;
-    act.sa_restorer = 0;
-    sigemptyset(&act.sa_mask);
-    sigaction(SIGSEGV, &act, NULL);
 
     handle = nx_function_begin( NX_FUNC_COMP_GZIP, 0);
     if (!handle) {

samples/openmultiple.c
@@ -11,7 +11,6 @@
 #include <sys/ioctl.h>
 #include <endian.h>
 #include <pthread.h>
-#include <signal.h>
 #include <dirent.h>
 
 #include <sys/stat.h>

samples/samples_utils.c
@@ -12,8 +12,6 @@
 
 #define hwsync()    asm volatile("sync" ::: "memory")
 
-void *_nx_fault_storage_address;
-
 static int _nx_wait_ticks(uint64_t accumulated_ticks)
 {
 	uint64_t ts, te, mhz, us;
@@ -41,11 +39,6 @@ int _nx_wait_for_csb(nx_gzip_crb_cpb_t *cmdp)
 
 		if (t > (60 * onesecond)) /* 1 min */
 			break;
-
-		/* fault address from signal handler */
-		if (_nx_fault_storage_address) {
-			return -EAGAIN;
-		}
 
 		hwsync();
 	} while (getnn(cmdp->crb.csb, csb_v) == 0);
@@ -78,10 +71,6 @@ int _nxu_run_job(nx_gzip_crb_cpb_t *cmdp, nx_devp_t nxhandle)
 			ret = _nx_wait_for_csb( cmdp );
 
 			if (ret == -EAGAIN) {
-				volatile long x;
-				x = *(long *)_nx_fault_storage_address;
-				*(long *)_nx_fault_storage_address = x;
-				_nx_fault_storage_address = 0;
 				continue;
 			}
 			else

samples/samples_utils.h
@@ -12,8 +12,6 @@ typedef struct top_sym_t {
 
 #define llns 0
 
-extern void *_nx_fault_storage_address;
-
 int _nxu_run_job(nx_gzip_crb_cpb_t *cmdp, void *handle);
 int _nx_touch_pages(void *buf, long buf_len, long page_len, int wr);
 int _nx_touch_pages_dde(nx_dde_t *ddep, long buf_sz, long page_sz, int wr);

samples/simpleapi/gzip_simple.c
@@ -18,14 +18,6 @@ __attribute__((constructor)) void nx_init(void)
 		nx_devices[ctr]->open_count = 0;
 		nx_devices[ctr]->chipId = ctr;
 	}
-
-	/* add a signal action */
-	sigact.sa_handler = 0;
-	sigact.sa_sigaction = sigsegv_handler;
-	sigact.sa_flags = SA_SIGINFO;
-	sigact.sa_restorer = 0;
-	sigemptyset(&sigact.sa_mask);
-	sigaction(SIGSEGV, &sigact, NULL);
 }
 
 __attribute__((destructor)) void nx_deinit(void)
@@ -252,14 +244,6 @@ int nx_append_dde(nx_dde_t *ddl, void *addr, uint32_t len)
 		putp32(ddl, ddebc, bytes); /* byte sum of all dde */
 	}
 	return bytes;
-}
-
-void sigsegv_handler(int sig, siginfo_t *info, void *ctx)
-{
-	fprintf(stderr, "%d: Got signal %d si_code %d, si_addr %p\n", getpid(),
-		sig, info->si_code, info->si_addr);
-	nx_fault_storage_address = info->si_addr;
-	// exit(0);
 }
 
 static void nx_print_dde(nx_dde_t *ddep, const char *msg)

samples/simpleapi/gzip_simple.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
 #include <syscall.h>
 #include <pthread.h>
 #include "nx.h"
@@ -29,12 +28,9 @@ extern void *nx_function_begin(int function, int pri);
 extern int nx_function_end(void *handle);
 
 struct sigaction sigact;
-void *nx_fault_storage_address;
 
 void *nx_overflow_buffer;
 p9_simple_handle_t *nx_devices[NX_MAX_DEVICES];
-
-void sigsegv_handler(int sig, siginfo_t *info, void *ctx);
 
 /*open the device*/
 p9_simple_handle_t *p9open();

test/libnxz.abi
@@ -89,7 +89,6 @@
     <typedef-decl name='__uint64_t' type-id='7359adad' id='8910171f'/>
     <pointer-type-def type-id='48b5725f' size-in-bits='64' id='eaa32e2f'/>
     <var-decl name='tb_freq' type-id='9c313c2d' visibility='default'/>
-    <var-decl name='nx_fault_storage_address' type-id='eaa32e2f' visibility='default'/>
   </abi-instr>
   <abi-instr address-size='64' path='nx_adler32.c' language='LANG_C11'>
     <type-decl name='long int' size-in-bits='64' id='bd54fe1a'/>